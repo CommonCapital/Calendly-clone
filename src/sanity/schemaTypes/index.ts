7dad2cc7c99b1ef716cd507563f2bdb1d39886de
CommonCapital <nursan2007@gmail.com>
2026-04-13 17:12:24 +0500

    add

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,10 +1,18 @@
-import { type SchemaTypeDefinition } from 'sanity'
-
-import {blockContentType} from './blockContentType'
-import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
+import { type SchemaTypeDefinition } from "sanity";
+import { userType } from "./userType";
+import { availabilitySlotType } from "./availabilitySlotType";
+import { connectedAccountType } from "./connectedAccountType";
+import { bookingType } from "./bookingType";
+import { meetingTypeType } from "./meetingTypeType";
+import { feedbackType } from "./feedbackType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType],
-}
+  types: [
+    userType,
+    availabilitySlotType,
+    connectedAccountType,
+    bookingType,
+    meetingTypeType,
+    feedbackType,
+  ],
+};
